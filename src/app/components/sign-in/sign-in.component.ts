c6f88eb58866b5bdc4d5717cb46843c37ce3df6f
AlymAlym <alymalym@web.de>
2020-03-27 12:56:35 +0100

    Login Component + settings + additional components

---
+++ src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+//import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from '../../shared/services/auth.service';
+import * as firebase from '@firebase/app';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+
+export class SignInComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  ngOnInit() { }
+}
