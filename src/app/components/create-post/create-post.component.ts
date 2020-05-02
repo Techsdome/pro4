ad0b5ef7991f4b0bb9fc57ea0328e2953b3c3b03
damir <damirmarkovic13@yahoo.com>
2020-05-02 19:08:19 +0200

    commenting

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -81,6 +81,10 @@ export class CreatePostComponent implements OnInit {
                         uid: this.authService.afAuth.auth.currentUser.uid,
                         photoURL: tempPhotoUrl,
                         displayName: `${val.firstname} ${val.lastname}`
+                    }).then(docRef => {
+                        this.authService.afs.doc(`generalPosts/allPosts`).collection('post').doc(docRef.id).update({
+                            postId: docRef.id
+                        });
                     });
                 });
         });
--- src/app/components/general-post/general-post.component.ts
+++ src/app/components/general-post/general-post.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DataServiceService} from '../../shared/services/data-service.service';
+import {AuthService} from '../../shared/services/auth.service';
 
 @Component({
     selector: 'app-general-post',
@@ -16,15 +18,53 @@ export class GeneralPostComponent implements OnInit {
     @Input() postSecond: string;
     @Input() postDisplayName: string;
     @Input() postPhotoUrl: string;
+    @Input() postId: string;
+    comment: string;
+    allComments: {}[];
+    showCommentSection = false;
+    commentsLenght: number;
 
-    constructor() {
+    constructor(private dataService: DataServiceService, private authService: AuthService) {
     }
 
-    ngOnInit(): void {
+    addComment() {
+        this.authService.getCurrentUser().subscribe((result) => {
+            this.authService.afs.collection('users').doc(result.uid).valueChanges()
+                .subscribe((val: any) => {
+                    this.authService.afs.doc(`generalPosts/allPosts/post/${this.postId}`).collection('comments').add({
+                        comment: this.comment,
+                        commentName: val.firstname + val.lastname
+                    });
+                    this.comment = '';
+                });
+        });
+    }
+
+    openCommentSection() {
+        this.showCommentSection = !this.showCommentSection;
     }
 
     openComment() {
         this.edit = !this.edit;
     }
+
+    ngOnInit(): void {
+        this.authService.afs.doc(`generalPosts/allPosts/post/${this.postId}`).collection('comments').valueChanges()
+            .subscribe((val) => {
+                this.allComments = [];
+                val.forEach((value) => {
+                    const commentObject = {
+                        text: value.comment,
+                        name: value.commentName
+                    };
+                    this.allComments.push(commentObject);
+                    this.commentsLenght = this.allComments.length;
+                });
+            });
+        if (this.commentsLenght === undefined) {
+            this.commentsLenght = 0;
+        }
+    }
+
 
 }
