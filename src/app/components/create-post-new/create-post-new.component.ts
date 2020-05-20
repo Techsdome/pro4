c18975f6136355d9f9e989d74433701736ba10aa
damir <damirmarkovic13@yahoo.com>
2020-05-20 20:58:03 +0200

    quill ffehler

---
+++ src/app/components/create-post-new/create-post-new.component.ts
@@ -0,0 +1,136 @@
+import {Component, OnInit} from '@angular/core';
+import {DataServiceService} from '../../shared/services/data-service.service';
+import {AuthService} from '../../shared/services/auth.service';
+import {Item} from '../../models/Item';
+import {User} from '../../shared/services/user';
+import {FormControl, FormGroup} from '@angular/forms';
+
+
+@Component({
+    selector: 'app-create-post-new',
+    templateUrl: './create-post-new.component.html',
+    styleUrls: ['./create-post-new.component.css']
+})
+export class CreatePostNewComponent implements OnInit {
+
+    showScreen = false;
+    items: Item[];
+    user: User;
+    posts: string[];
+    post: string;
+
+    editorForm: FormGroup;
+
+    editorStyle = {
+        justifyContent: 'center',
+        alignContent: 'center',
+        height: '200px',
+        width: '100%',
+        backgroundColor: 'white',
+    };
+
+    config = {
+        toolbar: [
+            ['bold', 'italic', 'underline', 'size'],
+            ['blockquote', 'code-block', 'link'],
+        ]
+    };
+
+    constructor(private dataService: DataServiceService, private authService: AuthService) {
+    }
+
+    getExtendedData(item) {
+        for (const it in item) {
+            if (this.user.uid === item[it].uid) {
+                this.posts = item[it].posts;
+            }
+        }
+    }
+
+    savePost() {
+        this.updatePostsFirebase(this.post);
+    }
+
+    updatePostsFirebase(postParam) {
+        const date: Date = new Date();
+        this.authService.afs.doc(`users/${this.authService.afAuth.auth.currentUser.uid}`).collection('posts').add({
+            post: this.post,
+            date: date.toLocaleDateString(),
+            day: date.getUTCDate(),
+            month: (date.getUTCMonth() + 1),
+            year: date.getUTCFullYear(),
+            hour: date.getHours(),
+            minutes: date.getMinutes(),
+            second: date.getSeconds()
+        });
+        let tempPhotoUrl: string;
+        let tempDisplayName: string;
+        let tempFirstName: string;
+        let tempLastName: string;
+
+        this.authService.getCurrentUser().subscribe((result) => {
+            this.user = result;
+            this.authService.afs.collection('users').doc(result.uid).valueChanges()
+                .subscribe((val: any) => {
+                    tempFirstName = val.firstname;
+                    tempLastName = val.lastname;
+                    if (this.authService.afAuth.auth.currentUser.photoURL === undefined ||
+                        this.authService.afAuth.auth.currentUser.photoURL === null) {
+                        tempPhotoUrl = 'https://d2x5ku95bkycr3.cloudfront.net/App_Themes/Common/images/profile/0_200.png';
+                    } else {
+                        tempPhotoUrl = this.authService.afAuth.auth.currentUser.photoURL;
+                    }
+                    if (this.authService.afAuth.auth.currentUser.displayName === undefined ||
+                        this.authService.afAuth.auth.currentUser.displayName === null) {
+                        tempDisplayName = tempFirstName + ' ' + tempLastName;
+                    } else {
+                        tempDisplayName = this.authService.afAuth.auth.currentUser.displayName;
+                    }
+
+                    this.authService.afs.doc(`generalPosts/allPosts`).collection('post').add({
+                        post: postParam,
+                        date: date.toLocaleDateString(),
+                        day: date.getUTCDate(),
+                        month: (date.getUTCMonth() + 1),
+                        year: date.getUTCFullYear(),
+                        hour: date.getHours(),
+                        minutes: date.getMinutes(),
+                        second: date.getSeconds(),
+                        uid: this.authService.afAuth.auth.currentUser.uid,
+                        photoURL: tempPhotoUrl,
+                        displayName: `${val.firstname} ${val.lastname}`
+                    }).then(docRef => {
+                        this.authService.afs.doc(`generalPosts/allPosts`).collection('post').doc(docRef.id).update({
+                            postId: docRef.id
+                        });
+                    });
+                });
+        });
+
+    }
+
+    toggleScreen() {
+        this.showScreen = !this.showScreen;
+        this.post = '';
+    }
+
+    ngOnInit(): void {
+        this.dataService.getItems().subscribe(items => {
+            this.items = items;
+            this.getExtendedData(items);
+        });
+
+        this.dataService.getCurrentUser().subscribe(user => {
+            this.user = user;
+        });
+        this.editorForm = new FormGroup({
+            editor: new FormControl(null)
+        });
+    }
+
+    maxLength(e) {
+        if (e.editor.getLength() > 1000) {
+            e.editor.deleteText(10, e.editor.getLength());
+        }
+    }
+}
--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -13,99 +13,110 @@ import {UtilitiesService} from '../../app.component';
 import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {trigger, state, style, animate, transition, keyframes, query, stagger} from '@angular/animations';
 import {NewQuestionComponent} from '../new-question/new-question.component';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
 import {FormControl} from '@angular/forms';
+import {CreatePostNewComponent} from "../create-post-new/create-post-new.component";
 
 
 @Component({
-  selector: 'app-main-navbar',
-  templateUrl: './main-navbar.component.html',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./main-navbar.component.css'],
-  animations: [
-    trigger('slide', [
-      transition(':enter', [
-        style({transform: 'translate(0px, 150px)'}),
-        animate('1s')
-      ]),
-    ]),
-  ]
+    selector: 'app-main-navbar',
+    templateUrl: './main-navbar.component.html',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./main-navbar.component.css'],
+    animations: [
+        trigger('slide', [
+            transition(':enter', [
+                style({transform: 'translate(0px, 150px)'}),
+                animate('1s')
+            ]),
+        ]),
+    ]
 })
 export class MainNavbarComponent implements OnInit {
-  public status = false;
-  public menuClicked = false;
-  user: Observable<User>;
-  items: Item[];
-  photoURL: string;
-  stat: string;
+    public status = false;
+    public menuClicked = false;
+    user: Observable<User>;
+    items: Item[];
+    photoURL: string;
+    stat: string;
 
-  myControl = new FormControl();
-  results: Observable<any[]>;
-  offset = new Subject<string>();
+    myControl = new FormControl();
+    results: Observable<any[]>;
+    offset = new Subject<string>();
 
 
-  constructor(@Inject(AuthService) public authService: AuthService,
-              @Inject(DataServiceService) private dataService: DataServiceService,
-              public addproject: NewProjectComponent, private utilitiesService: UtilitiesService,
-              public activeModal: NgbActiveModal, private modalService: NgbModal, private afs: AngularFirestore) {
-  }
+    constructor(@Inject(AuthService) public authService: AuthService,
+                @Inject(DataServiceService) private dataService: DataServiceService,
+                public addproject: NewProjectComponent, private utilitiesService: UtilitiesService,
+                public activeModal: NgbActiveModal, private modalService: NgbModal, private afs: AngularFirestore) {
+    }
 
-  onkeyup(e) {
-    this.offset.next(e.target.value.toLowerCase());
-  }
+    onkeyup(e) {
+        this.offset.next(e.target.value.toLowerCase());
+    }
 
-  search() {
-    return this.offset.pipe(
-      filter(val => !!val),
-      switchMap(offset => {
-        return this.afs.collection('users', ref =>
-          ref.orderBy(`searchableIndex.${offset}`).limit(5)
-        ).valueChanges();
-      })
-    );
-  }
+    search() {
+        return this.offset.pipe(
+            filter(val => !!val),
+            switchMap(offset => {
+                return this.afs.collection('users', ref =>
+                    ref.orderBy(`searchableIndex.${offset}`).limit(5)
+                ).valueChanges();
+            })
+        );
+    }
 
-  Popen() {
-    const modalRef = this.modalService.open(NewProjectComponent,
-      {
-        scrollable: true,  beforeDismiss: () => {
-          return confirm('Do you want to lose your changes?');
-        }
-      });
-    // modalRef.componentInstance.name = 'World';
-  }
+    Popen() {
+        const modalRef = this.modalService.open(NewProjectComponent,
+            {
+                scrollable: true, beforeDismiss: () => {
+                    return confirm('Do you want to lose your changes?');
+                }
+            });
+        // modalRef.componentInstance.name = 'World';
+    }
 
-  Qopen() {
-    const modalRef = this.modalService.open(NewQuestionComponent,
-      {
-        scrollable: true,  beforeDismiss: () => {
-          return confirm('Do you want to lose your changes?');
-        }
-      });
-    // modalRef.componentInstance.name = 'World';
-  }
+    Qopen() {
+        const modalRef = this.modalService.open(NewQuestionComponent,
+            {
+                scrollable: true, beforeDismiss: () => {
+                    return confirm('Do you want to lose your changes?');
+                }
+            });
+        // modalRef.componentInstance.name = 'World';
+    }
 
-  animateOn() {
-    this.status = !this.status;
-  }
+    animateOn() {
+        this.status = !this.status;
+    }
 
-  showMenuBar() {
-    this.menuClicked = !this.menuClicked;
-  }
+    showMenuBar() {
+        this.menuClicked = !this.menuClicked;
+    }
 
-  ngOnInit() {
-    this.results = this.search();
-    this.authService.afAuth.authState.subscribe(user => {
-      if (user) {
-        this.photoURL = user.photoURL;
-      }
-    });
+    createNewPost() {
+        const openPost = this.modalService.open(CreatePostNewComponent,
+            {
+                scrollable: true, beforeDismiss: () => {
+                    return confirm('Do you want to lose your changes?');
+                }
+            });
+        // modalRef.componentInstance.name = 'World';
+    }
 
-  }
+    ngOnInit() {
+        this.results = this.search();
+        this.authService.afAuth.authState.subscribe(user => {
+            if (user) {
+                this.photoURL = user.photoURL;
+            }
+        });
+
+    }
 
 // const directory = document.querySelector('.bubble').getBoundingClientRect();
 
-  slideIt(dat) {
-    this.stat = dat;
-  }
+    slideIt(dat) {
+        this.stat = dat;
+    }
 }
