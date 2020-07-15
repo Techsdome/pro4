9ec832548401cfcaa3d39d0aa256bb94c7a32478
damir <damirmarkovic13@yahoo.com>
2020-07-16 01:10:35 +0200

    main feed debugging | add comments section

--- src/app/components/show-projects/show-projects.component.ts
+++ src/app/components/show-projects/show-projects.component.ts
@@ -7,9 +7,9 @@ import {Posts} from '../../shared/services/posts';
 import {PostsService} from '../../services/posts.service';
 
 @Component({
-  selector: 'app-show-projects',
-  templateUrl: './show-projects.component.html',
-  styleUrls: ['./show-projects.component.css']
+    selector: 'app-show-projects',
+    templateUrl: './show-projects.component.html',
+    styleUrls: ['./show-projects.component.css']
 })
 export class ShowProjectsComponent implements OnInit {
 
@@ -23,10 +23,8 @@ export class ShowProjectsComponent implements OnInit {
     commentsLenght: number;
     postId: string;
     posts: any[] = [];
-
     activeMenu: string;
     filter: boolean;
-
 
     constructor(public authservice: AuthService, private postService: PostsService) {
     }
@@ -37,9 +35,8 @@ export class ShowProjectsComponent implements OnInit {
 
     openComment() {
         this.edit = !this.edit;
-        this.showCommentSection = !this.showCommentSection;
     }
-/*
+
     addComment() {
         this.authservice.getCurrentUser().subscribe((result) => {
             this.authservice.afs.collection('users').doc(result.uid).valueChanges()
@@ -50,47 +47,29 @@ export class ShowProjectsComponent implements OnInit {
                     });
                     this.comments = [];
                 });
-        })
-    }*/
-
-    addComment() {
-        this.authservice.getCurrentUser().subscribe((result) => {
-            this.authservice.afs.collection('users').doc(result.uid).valueChanges()
-                .subscribe((val: any) => {
-                    this.authservice.afs.doc(`mainFeed/allPosts/post/${this.postId}`).collection('comments').add({
-                        comment: this.comment,
-                        commentName: val.firstname + val.lastname
-                    });
-                    this.comment = '';
-                });
         });
     }
 
 
-
-
-
-
-
-
     ngOnInit(): void {
-      this.activeMenu = '';
-            this.changeMenuItem(this.activeMenu);
+        this.activeMenu = '';
+        this.changeMenuItem(this.activeMenu);
         this.authservice.afs.collection(`mainFeed/allPosts/post/${this.allPostsObject.postId}/comments`).valueChanges()
             .subscribe((comment) => {
                 this.commentsLenght = comment.length;
                 comment.forEach(cmt => {
                     this.comments.push(cmt);
-                    console.log(cmt)
+                    console.log(cmt);
                 });
             });
     }
-  toggle() {
-    this.filter = !this.filter;
-  }
+
+    toggle() {
+        this.filter = !this.filter;
+    }
 
 
-  changeMenuItem(event) {
-    this.posts = this.postService.getPosts(event);
-  }
+    changeMenuItem(event) {
+        this.posts = this.postService.getPosts(event);
+    }
 }
